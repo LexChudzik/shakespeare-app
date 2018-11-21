d92e28e34ae74914667f425788d338a95e2617d8
Lex Chudzik <chudzik.alex@gmail.com>
2018-11-21 15:22:38 -0600

    sql reqests to add productions

--- server/routes/production.router.js
+++ server/routes/production.router.js
@@ -49,5 +49,57 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//add live production
+router.post('/live', (req, res) => {
+    const p = req.body;
+    //delete properties with blank strings from object
+    Object.keys(p).forEach((key) => (p[key] === '') && delete p[key]);
+    const sqlText = `WITH ins_1 AS (
+        INSERT INTO production (play_id, medium)
+        VALUES ($1, 'film')
+        RETURNING id 
+        )
+    , ins_2 AS (
+        INSERT INTO live (production_id, location, start_date, end_date, url, image_url)
+        SELECT id, $2, $3, $4, $5, $6 FROM ins_1
+        RETURNING production_id
+        )
+    SELECT id FROM ins_1;`;
+    pool.query(sqlText, [p.play_id, p.location, p.start_date, p.end_date, p.url, p.image_url])
+    .then((result) => {
+        res.send(results.rows);
+    })
+    .catch((error) => {
+        console.log('error making database query', sqlText, error);
+        res.sendStatus(500);
+    })
+})
+
+//add filmed production
+router.post('/film', (req, res)=>{
+    const p = req.body;
+    //delete properties with blank strings from object
+    Object.keys(p).forEach((key) => (p[key] === '') && delete p[key]);
+    const sqlText = `WITH ins_1 AS (
+        INSERT INTO production (play_id, medium)
+        VALUES ($1, 'film')
+        RETURNING id 
+        )
+    , ins_2 AS (
+        INSERT INTO film (production_id, release_date, title, poster_path, loose_adapt, tmdb_id)
+        SELECT id, $2, $3, $4, $5, $6 FROM ins_1
+        RETURNING production_id
+        )
+    SELECT id FROM ins_1;`;  
+    pool.query(sqlText, [p.playid, p.release_date, p.title, p.poster_path, p.loose_adapt, p.tmdb_id])
+    .then((result) => {
+        res.send(results.rows);
+    })
+    .catch((error) => {
+        console.log('error making database query', sqlText, error);
+        res.sendStatus(500);
+    })
+})
+
 module.exports = router;
 
