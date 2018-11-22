6812bb1c9818ae386482a03cd7d5b0c1acf838db
Lex Chudzik <chudzik.alex@gmail.com>
2018-11-21 22:06:57 -0600

    adding film routes

--- src/redux/sagas/filmSaga.js
+++ src/redux/sagas/filmSaga.js
@@ -4,25 +4,25 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 function* searchMovies(action) {
     try {
       const response = yield call( axios.post, '/api/tmdb', action.payload);
-      yield put( {type: 'SET_RESULTS', payload: response.data} );
+      yield put( {type: 'SET_TMDB_RESULTS', payload: response.data} );
     }
     catch (error) {
       console.log('error with giphy search', error);
     }
   }
   
-  function* saveMovie(action) {
-    try {
-      yield call( axios.post, '/api/tmdb', action.payload);
-      //yield put( {type: 'SET_RESULTS', payload: response.data} );
-    }
-    catch (error) {
-      console.log('error with giphy search', error);
-    }
-  }
+//   function* saveMovie(action) {
+//     try {
+//       yield call( axios.post, '/api/tmdb', action.payload);
+//       //yield put( {type: 'SET_RESULTS', payload: response.data} );
+//     }
+//     catch (error) {
+//       console.log('error with giphy search', error);
+//     }
+//   }
 
 function* filmSaga() {
-    yield takeEvery( 'SEND_SAVE', saveMovie );
+   // yield takeEvery( 'SEND_SAVE', saveMovie );
     yield takeEvery( 'SEARCH_MOVIES',  searchMovies);
 }
 
