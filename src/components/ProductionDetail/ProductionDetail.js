1ab9502bbd2b2c766e34377954ffddcb0be600e7
Lex Chudzik <chudzik.alex@gmail.com>
2018-12-02 14:41:47 -0600

    production card styled

--- src/components/ProductionDetail/ProductionDetail.js
+++ src/components/ProductionDetail/ProductionDetail.js
@@ -20,34 +20,40 @@ class ProductionDetail extends Component {
   render() {
     return (
       <div className="card">
-        {this.props.p.film_title && (this.props.p.film_title !== this.props.p.play_title) &&<h2>{this.props.p.film_title}</h2>}
 
-        <h3>{this.props.p.play_title}</h3>
+        {/* image */}
+        {(this.props.p.poster_path || this.props.p.image_url) &&
+        <div className="card-image">
+          {this.props.p.poster_path && <img alt="poster" src={`https://image.tmdb.org/t/p/w500/${this.props.p.poster_path}`}/>}
+          {this.props.p.image_url && <img alt="poster" src={this.props.p.image_url}/>}
+        </div>}
 
-        {this.props.p.poster_path && <img alt="poster" src={`https://image.tmdb.org/t/p/w500/${this.props.p.poster_path}`}/>}
+          {/* title */}
+        <div className="card-title">
+          {this.props.p.film_title && (this.props.p.film_title !== this.props.p.play_title) &&<h2>{this.props.p.film_title}</h2>}
+          <h3>{this.props.p.play_title}</h3>
+        </div>
 
-        {this.props.p.image_url && <img alt="poster" src={this.props.p.image_url}/>}
+        <div className="card-subtitle">
+          {this.props.p.start_date && this.props.p.end_date &&
+            <p>{this.dateFormat(this.props.p.start_date)} - {this.dateFormat(this.props.p.end_date)}</p>}
+          {this.props.p.release_date && <p>{this.dateFormat(this.props.p.release_date)}</p>}
+          {this.props.p.theater && <p>{this.props.p.theater}</p>}
+          {this.props.p.location && <p>{this.props.p.location}</p>}
+          {this.props.p.production_url && <a href={this.props.p.production_url} rel="noopener noreferrer" target="_blank">theater website</a>}
+        </div>
 
-        {this.props.p.start_date && this.props.p.end_date &&
-          <p>{this.dateFormat(this.props.p.start_date)} - {this.dateFormat(this.props.p.end_date)}</p>}
-        
-        {this.props.p.theater && <p>{this.props.p.theater}</p>}
+        <div className="card-history">
+          {this.props.p.date && <p>View Date: {this.dateFormat(this.props.p.date)} </p> }
+          {this.props.p.rating && <Rating initialRating={this.props.p.rating} readonly/>}
+          {this.props.p.comments && <p>{this.props.p.comments}</p>}
+        </div>
 
-        {this.props.p.location && <p>{this.props.p.location}</p>}
+        <div className="card-buttons">
+          {!this.props.p.viewing_id && <ToggleListButton p={this.props.p}/>} 
+          {!this.props.p.viewing_id && <button onClick={this.goToLog}>LOG VIEW</button>}
+        </div>
 
-        {this.props.p.release_date && <p>{this.dateFormat(this.props.p.release_date)}</p>}
-
-        {this.props.p.production_url && <a href={this.props.p.production_url} rel="noopener noreferrer" target="_blank">theater site</a>}
-
-        {this.props.p.date && <p>View Date: {this.dateFormat(this.props.p.date)} </p> }
-
-        {this.props.p.rating && <Rating initialRating={this.props.p.rating} readonly/>}
-
-        {this.props.p.comments && <p>{this.props.p.comments}</p>}
-
-        {!this.props.p.viewing_id && <button onClick={this.goToLog}>LOG VIEW</button>}
-
-        {!this.props.p.viewing_id && <ToggleListButton p={this.props.p}/>}
       </div>
   )}
 }
