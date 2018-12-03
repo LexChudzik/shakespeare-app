6dc31ef789f2396180d5bea945b9c279b412c0b4
Lex Chudzik <chudzik.alex@gmail.com>
2018-12-03 16:36:05 -0600

    revert

--- server/server.js
+++ server/server.js
@@ -40,16 +40,6 @@ app.use('/api/list', listRouter);
 app.use('/api/detail', detailRouter);
 
 // Serve static files
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 app.use(express.static('build'));
 
 // App Set //
