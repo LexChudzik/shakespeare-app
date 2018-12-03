4a815aa6a03a0e68d8ecb79285a97e4ee48f8991
Lex Chudzik <chudzik.alex@gmail.com>
2018-12-03 16:22:04 -0600

    sever for heroku

--- server/server.js
+++ server/server.js
@@ -41,6 +41,16 @@ app.use('/api/detail', detailRouter);
 
 // Serve static files
 app.use(express.static('build'));
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // App Set //
 const PORT = process.env.PORT || 5000;
